303b06aa89ed005f8116a9c8cf632b221886b345
Alexandre Desroches <alexandre_d_7@hotmail.com>
2020-08-11 10:07:26 -0400

    -added drop-shadow on signature logo

--- src/index.js
+++ src/index.js
@@ -14,18 +14,27 @@ import bg3 from "./images/bg3.png";
 
 window.onload = () => {
     document.querySelector(".welcome .signature").src = signatureImg;
-    registerAutoBackgroundChanger();
+    initBackgroundChangerClosure();
 }
 
-function registerAutoBackgroundChanger() {
+function initBackgroundChangerClosure() {
     let backgroundIndex = 0;
     const backgrounds = [bg1, bg2, bg3];
-
+    const lazyLoadPlaceholder = document.getElementById("invisibleLazyLoadSrcPlaceholder");
+    //initial background image is loaded now then inside the interval
     document.body.style.backgroundImage = `url('${backgrounds[backgroundIndex]}')`;
-    setInterval(autoChangeBackgroundImg, 2300);
+    setInterval(function backgroundChangerClosure() {
 
-    function autoChangeBackgroundImg() {
         backgroundIndex = backgroundIndex >= (backgrounds.length - 1) ? 0 : backgroundIndex + 1;
-        document.body.style.backgroundImage = `url('${backgrounds[backgroundIndex]}')`;
-    }
+        const img = backgrounds[backgroundIndex];
+        //force GET image from server before changing the actual visible background:
+        lazyLoadPlaceholder.src = img;
+        setTimeout(() => {
+            //wait until background image is loaded from network
+            document.body.style.backgroundImage = `url('${img}')`;
+        }, 1000)
+
+        const signature = document.querySelector(".welcome .signature");
+        signature.style.filter = `drop-shadow(0.3rem 0.3rem 0.4rem hsla(${Math.random() * 360}, 100%, 48%, ${Math.random()}))`;
+    }, 2300);
 }
