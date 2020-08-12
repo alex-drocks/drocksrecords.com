88ec7e7b38547e2921465abfcdf37f66a2acdcd2
Alexandre Desroches <alexandre_d_7@hotmail.com>
2020-08-12 09:53:49 -0400

    - Refactored index.js to use object oriented syntax - Fully working auto-color changers

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ import signatureImg from "./images/welcome-signature-logo.png"
 import bg1 from "./images/bg1.png";
 import bg2 from "./images/bg2.png";
 import bg3 from "./images/bg3.png";
-import bg5 from "./images/bg5.png";
 
 
 window.onload = () => {
     registerListeners();
-    initBackgroundChanger();
-    initSignatureColorChanger();
+    background.init();
+    signature.init();
 }
 
 function registerListeners() {
@@ -26,38 +25,84 @@ function registerListeners() {
         btn.onclick = () => {
             content.classList.add("shown");
             welcome.classList.add("navbar");
+
+            signature.unregisterAnimation();
         };
     });
 }
 
-function initBackgroundChanger() {
-    const backgrounds = [bg1, bg2, bg3, bg5];
-    const lazyLoadPlaceholder = document.getElementById("invisibleLazyLoadSrcPlaceholder");
+const background = {
+    lazyLoadPlaceholder: document.getElementById("invisibleLazyLoadSrcPlaceholder"),
+    images: [bg1, bg2, bg3],
+    currIndex: 1,
+    image: undefined,
+    intervalID: undefined,
+    init() {
+        //initial background image is loaded now then inside the interval
+        background.image = background.images[1];
+        document.body.style.backgroundImage = `url('${background.image}')`;
+        background.registerAnimation();
+        //force immediate animation transition
+        background.setImage();
 
-    //initial background image is loaded now then inside the interval
-    let img = backgrounds[getRandomIndex(backgrounds)];
-    document.body.style.backgroundImage = `url('${img}')`;
-    setInterval(function backgroundChangerClosure() {
-        img = backgrounds[getRandomIndex(backgrounds)]
+        document.querySelector(".welcome .buttons").style.transform = "translateX(0)";
+    },
+    registerAnimation() {
+        //start animating drop-shadow color
+        background.intervalID = setInterval(() => {
+            background.setImage();
+        }, 3300);
+    },
+    setImage() {
+        background.image = background.getNextImage();
         //force GET image from server before changing the actual visible background:
-        lazyLoadPlaceholder.src = img;
+        background.lazyLoadPlaceholder.src = background.image;
         setTimeout(() => {
             //wait until background image is loaded from network
-            document.body.style.backgroundImage = `url('${img}')`;
-        }, 1200)
-    }, 3000);
+            document.body.style.backgroundImage = `url('${background.image}')`;
+        }, 1500)
+    },
+    getNextImage() {
+        background.currIndex = getNextArrIndex(background.images, background.currIndex);
+        console.log(background.currIndex)
+        return background.images[background.currIndex];
+    },
 }
 
-function initSignatureColorChanger() {
-    const colors = ["000000", "dd00ff", "0000ff", "ffffff", "ff0000"];
-    const signature = document.querySelector(".welcome .signature");
+const signature = {
+    html: document.querySelector(".welcome .signature"),
+    colors: ["1010ff", "dd00ff", "1095ff", "101010", "ff0010", "8080ff"],
+    currIndex: -1,
+    intervalID: undefined,
+    init() {
+        signature.html.src = signatureImg;
+        signature.setDropShadowColor();
+        signature.registerAnimation();
 
-    signature.src = signatureImg;
-    setInterval(function signatureColorChangerClosure() {
-        signature.style.filter = `drop-shadow(0.3rem 0.3rem 0.29rem #${colors[getRandomIndex(colors)]})`;
-    }, 3000);
+        signature.html.style.transform = "translateY(0)";
+    },
+    registerAnimation() {
+        //start animating drop-shadow color
+        signature.intervalID = setInterval(() => {
+            signature.setDropShadowColor();
+        }, 3000);
+    },
+    unregisterAnimation() {
+        //stop animating drop-shadow color
+        clearInterval(signature.intervalID);
+        signature.html.style.filter = "none";
+    },
+    setDropShadowColor() {
+        signature.html.style.filter = `
+        drop-shadow(0.3rem 0.3rem 0.${Math.floor((Math.random() * 6) + 2)}rem #${signature.getNextColor()})
+        `
+    },
+    getNextColor() {
+        signature.currIndex = getNextArrIndex(signature.colors, signature.currIndex);
+        return signature.colors[signature.currIndex]
+    },
 }
 
-function getRandomIndex(arr) {
-    return Math.floor(Math.random() * arr.length);
+function getNextArrIndex(array, currIndex) {
+    return currIndex >= array.length - 1 ? 0 : currIndex + 1
 }
