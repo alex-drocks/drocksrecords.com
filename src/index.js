c596b5921cfc7882e5c641991b79500ce8d68601
Alexandre Desroches <alexandre_d_7@hotmail.com>
2020-08-17 22:50:02 -0400

    - finished desktop HD screen styles.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import {Nav} from "./lib/nav";
 
 //stylesheets
-import "./lib/common.css";
+import "./lib/base.css";
 import "./index.css";
 import "./lib/media-queries.css";
 import "./lib/nav.css";
@@ -18,85 +18,95 @@ import bg3 from "./images/bg3.png";
 
 
 window.onload = () => {
-    registerEventListeners();
-    background.init();
-    colorAnimation.init();
+    initialize();
 }
 
-function registerEventListeners() {
-    //define language from which button was clicked
-    let content;
+function initialize() {
+    backgroundImageChanger.init();
+    animations.init();
 
-    //welcome buttons actions
+    //welcome div
     const welcome = document.querySelector(".welcome");
-    welcome.querySelectorAll("button").forEach((btn) => {
-        btn.onclick = () => {
-            const contentLang = btn.dataset.lang;
-            content = setSelectedLanguageContent(contentLang);
+    welcome.classList.add("initialized");
 
-            content.classList.add("shown");
-            welcome.classList.add("has-been-clicked");
-            colorAnimation.unregisterAnimation();
-            // signature.html.style.display = "none";
+    //define language from which button was clicked
+    (function registerEventListeners() {
+        const punchLine = welcome.querySelector(".punch-line");
+        const signature = welcome.querySelector(".signature");
+        welcome.querySelectorAll("button").forEach((btn) => {
+            btn.onclick = () => {
+                const contentLang = btn.dataset.lang;
+                const content = setSelectedLanguageContent(contentLang);
+                content.classList.add("shown"); //show the actual content in the right language
 
-            //create right-side navbar like price feature
-            new Nav();
-            welcome.style.opacity = "0"; //trigger CSS opacity transition
-            setTimeout(() => {
-                //smooth switch signature to left side of screen
-                // signature.html.style.display = "";
-                welcome.style.justifyContent = "flex-start" //brutal change only at opacity of 0%
-                welcome.style.opacity = "1"
-                welcome.style.backgroundColor = "transparent";
-                document.querySelector(".welcome .background-fx").style.height = "635px"
+                welcome.classList.add("has-been-clicked");
+                // animations.unregisterAnimationTimer();
+                animations.shouldAnimateSignature = false;
 
-                //"Mixage, composition et formation audio..."
-                welcome.querySelector(".punch-line").style.display = "block";
+                //create right-side navbar like price feature
+                welcome.style.opacity = "0"; //trigger CSS opacity transition
+                signature.style.display = "none";
                 setTimeout(() => {
-                    welcome.querySelector(".punch-line").classList.add("shown");
-                    document.body.style.height = "";
-                    document.body.style.overflowY = "auto";
-                }, 200)
-            }, 1200);
+                    //smooth switch signature to left side of screen
+                    signature.style.display = "";
+                    welcome.style.justifyContent = "flex-start" //brutal change only at opacity of 0%
+                    welcome.style.opacity = "1"
+                    welcome.style.backgroundColor = "transparent";
 
-            function setSelectedLanguageContent(language) {
-                const otherHiddenLang = document.querySelector(
-                    `.content.lang-${language === "francais" ? "english" : "francais"}`
-                );
-                otherHiddenLang.remove();
-                setPunchLineText(language);
-                return document.querySelector(`.content.lang-${language}`);
+                    //"Mixage, composition et formation audio..."
+                    punchLine.style.display = "block";
+                    setTimeout(() => {
+                        punchLine.classList.add("shown");
+                        document.body.style.height = "";
+                        document.body.style.overflowY = "auto";
+                        const priceNavBar = new Nav();
+                        setTimeout(() => {
+                            priceNavBar.btn.classList.add("shown");
+                        }, 500)
+                    }, 200)
+                }, 1200);
 
-                function setPunchLineText(language) {
-                    const punchLine = document.querySelector(".welcome .punch-line");
-                    if (language === "francais") {
-                        punchLine.textContent = "Mixage, composition et formation audio...";
-                    } else if (language === "english") {
-                        punchLine.textContent = "Mixing, composition and audio teaching...";
+                function setSelectedLanguageContent(language) {
+                    const otherHiddenLang = document.querySelector(
+                        `.content.lang-${language === "francais" ? "english" : "francais"}`
+                    );
+                    otherHiddenLang.remove();
+                    setPunchLineText(language);
+                    return document.querySelector(`.content.lang-${language}`);
+
+                    function setPunchLineText(language) {
+                        if (language === "francais") {
+                            punchLine.textContent = "Mixage, composition et formation audio...";
+                        } else if (language === "english") {
+                            punchLine.textContent = "Mixing, composition and audio teaching...";
+                        }
                     }
                 }
-            }
-        };
-    });
+            };
+        });
 
-    //content cards actions
-    document.querySelectorAll(".card").forEach((card) => {
-        card.querySelector("button").onclick = () => {
-            card.classList.add("selected");
-            card.scrollIntoView({behavior: "smooth"})
-        };
-        const externLink = card.querySelector("a");
-        if (externLink) {
-            externLink.onclick = (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                openInNewTab(externLink.href);
+        //content cards actions
+        document.querySelectorAll(".card").forEach((card) => {
+            card.querySelector("button").onclick = toggleCardVisibility;
+
+            function toggleCardVisibility() {
+                card.classList.add("selected");
+                card.scrollIntoView({behavior: "smooth"})
             }
-        }
-    });
+
+            const externLink = card.querySelector("a");
+            if (externLink) {
+                externLink.onclick = (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    openInNewTab(externLink.href);
+                }
+            }
+        });
+    })();
 }
 
-const background = {
+const backgroundImageChanger = {
     lazyLoadPlaceholder: document.getElementById("invisibleLazyLoadSrcPlaceholder"),
     images: [bg1, bg2, bg3],
     currIndex: 1,
@@ -104,80 +114,81 @@ const background = {
     intervalID: undefined,
     init() {
         //initial background image is loaded now then inside the interval
-        background.image = background.images[1];
-        document.body.style.backgroundImage = `url('${background.image}')`;
-        background.registerAnimation();
+        backgroundImageChanger.image = backgroundImageChanger.images[1];
+        document.body.style.backgroundImage = `url('${backgroundImageChanger.image}')`;
+        backgroundImageChanger.registerAnimation();
         //force immediate animation transition
-        background.setImage();
-
-        document.querySelector(".welcome .background-fx").style.height = "80%"
-        document.querySelector(".welcome .buttons").style.transform = "translateX(0)";
+        backgroundImageChanger.setImage();
     },
     registerAnimation() {
         //start animating drop-shadow color
-        background.intervalID = setInterval(() => {
-            background.setImage();
+        backgroundImageChanger.intervalID = setInterval(() => {
+            backgroundImageChanger.setImage();
         }, 3000);
     },
     setImage() {
-        background.image = background.getNextImage();
+        backgroundImageChanger.image = backgroundImageChanger.getNextImage();
         //force GET image from server before changing the actual visible background:
-        background.lazyLoadPlaceholder.src = background.image;
+        backgroundImageChanger.lazyLoadPlaceholder.src = backgroundImageChanger.image;
         setTimeout(() => {
             //wait until background image is loaded from network
-            document.body.style.backgroundImage = `url('${background.image}')`;
-        }, 1500)
+            document.body.style.backgroundImage = `url('${backgroundImageChanger.image}')`;
+        }, 2000)
     },
     getNextImage() {
-        background.currIndex = getNextArrIndex(background.images, background.currIndex);
-        return background.images[background.currIndex];
+        backgroundImageChanger.currIndex = getNextArrIndex(backgroundImageChanger.images, backgroundImageChanger.currIndex);
+        return backgroundImageChanger.images[backgroundImageChanger.currIndex];
     },
 }
 
-const colorAnimation = {
+const animations = {
     html: {
         signature: document.querySelector(".welcome .signature"),
         bgFx: document.querySelector(".welcome .background-fx"),
     },
-    colors: ["1010ff", "dd00ff", "1095ff", "101010", "ff0010", "8080ff"],
+    colors: ["1010ff", "cc00ff", "1095ff", "101010", "ff0010", "8080ff"],
     currIndex: -1,
     intervalID: undefined,
+    shouldAnimateSignature: true,
+
     init() {
-        colorAnimation.html.signature.src = signatureImg;
-        colorAnimation.setDropShadowColor();
-        colorAnimation.registerAnimation();
-        colorAnimation.html.signature.style.transform = "translateY(0)";
+        animations.setNewColor();
+        animations.registerAnimationTimer();
     },
-    registerAnimation() {
+    registerAnimationTimer() {
         //start animating drop-shadow color
-        colorAnimation.intervalID = setInterval(() => {
-            colorAnimation.setDropShadowColor();
+        animations.intervalID = setInterval(() => {
+            animations.setNewColor();
         }, 3000);
     },
-    unregisterAnimation() {
+    unregisterAnimationTimer() {
         //stop animating drop-shadow color
-        clearInterval(colorAnimation.intervalID);
-        colorAnimation.html.signature.style.filter = "none";
-        colorAnimation.html.bgFx.style.borderTopColor = "var(--blue)";
+        clearInterval(animations.intervalID);
+        animations.html.signature.style.filter = "none";
+        animations.html.bgFx.style.borderTopColor = "var(--blue)";
     },
-    setDropShadowColor() {
-        const newColor = colorAnimation.getNextColor();
+    setNewColor() {
+        const newColor = getNextColor();
 
         //set signature logo contour color (drop-shadow)
-        colorAnimation.html.signature.style.filter = `
-        drop-shadow(0.3rem 0.3rem 0.${Math.floor((Math.random() * 6) + 2)}rem #${newColor})`;
+        if (animations.shouldAnimateSignature) {
+            animations.html.signature.style.filter = `
+            drop-shadow(0.3rem 0.3rem 0.${Math.floor((Math.random() * 6) + 2)}rem #${newColor})`;
+        }
 
         //set parallel bg lines color async with delayed timers
         setTimeout(() => {
-            colorAnimation.html.bgFx.style.borderTopColor = `#${newColor}`;
+            animations.html.bgFx.style.borderTopColor = `#${newColor}`;
             setTimeout(() => {
-                colorAnimation.html.bgFx.style.borderBottomColor = `#${newColor}`;
-            }, 2000)
-        }, 700)
-    },
-    getNextColor() {
-        colorAnimation.currIndex = getNextArrIndex(colorAnimation.colors, colorAnimation.currIndex);
-        return colorAnimation.colors[colorAnimation.currIndex]
+                animations.html.bgFx.style.borderBottomColor = `#${newColor}`;
+            }, 1700)
+        }, 500)
+
+        //closure
+        function getNextColor() {
+            animations.currIndex = getNextArrIndex(animations.colors, animations.currIndex);
+            return animations.colors[animations.currIndex]
+        }
     },
 }
 
